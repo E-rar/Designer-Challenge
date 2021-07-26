ad4e3ab893d48d8da9b0b3b7ec3be70229132292
erik-ramich <erik.ramich@gmx.de>
2021-07-26 19:37:50 +0200

    fixed pop-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function ValidateEmail(inputText)
 {
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 if(inputText.value.match(mailformat))
 {
 document.getElementById("subscribed").style.opacity=1;
@@ -14,4 +14,8 @@ document.getElementById("alert").style.opacity=1;
 document.form1.email.focus();
 return false;
 }
+}
+function closePopUp(){
+    document.getElementById("subscribed").style.opacity=0;
+    document.getElementById("subscribed").style.zIndex=0;
 }
