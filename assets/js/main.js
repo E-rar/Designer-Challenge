82f193684b343a6fd7a9a3114897f867e6a3328a
erik-ramich <erik.ramich@gmx.de>
2021-07-26 19:21:26 +0200

    pop-up

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function ValidateEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value.match(mailformat))
+{
+document.getElementById("subscribed").style.opacity=1;
+document.getElementById("subscribed").style.zIndex=1;
+document.form1.email.focus();
+return true;
+}
+else
+{
+document.getElementById("alert").style.opacity=1;
+document.form1.email.focus();
+return false;
+}
+}
